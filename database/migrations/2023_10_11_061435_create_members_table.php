ca38ab77299da2c144f5b89aa348ba4f7958ddb3
WenFanChang <d1104181101@gm.lhu.edu.tw>
2023-10-18 13:46:28 +0800

    避免mirgate命令產生的錯誤訊息

--- database/migrations/2023_10_11_061435_create_members_table.php
+++ database/migrations/2023_10_11_061435_create_members_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
-            $table->string("oid");
+            $table->integer("oid");
             $table->string("name");
             $table->float("height");
             $table->float("weight");
-            $table->string("year");
-            $table->string("age");
+            $table->integer("year");
+            $table->integer("age");
             $table->string("nationality");
 
             $table->timestamps();
