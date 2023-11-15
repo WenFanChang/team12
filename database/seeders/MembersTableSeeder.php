37bca08f41914b214b23c677c8b88e68d82e52dd
WenFanChang <d1104181101@gm.lhu.edu.tw>
2023-11-15 10:54:30 +0800

    新增seeder

---
+++ database/seeders/MembersTableSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MembersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    
+
+    public function generateRandomPosition() {
+        $position = ['主唱','舞者','吉他手','鼓手','鍵盤手','貝斯手','領唱','和聲','領舞','主舞','副唱','副Rapper','主Rapper'];
+        return $position[rand(0, count($position)-1)];
+
+    }
+
+    public function generateRandomNationality(){
+        $nationality = array(
+            '中國', '美國', '日本', '德國', '英國', '法國', '義大利', '俄羅斯', '加拿大', 
+            '南韓', '澳洲', '越南', '亞美尼亞', '紐西蘭', '瑞士', '馬來西亞'
+        );
+        return $nationality[rand(0,count($nationality)-1)];   
+    }
+    
+    public function run(){
+        $firstNames = ['John', 'Mary', 'Tom', 'Michael', 'David', 'Michael'];
+        $lastNames = ['Smith', 'Johnson', 'Williams', 'Lee', 'Beckham', 'Jordan'];
+        for ($i=0; $i<500; $i++) {
+            $name = $firstNames[array_rand($firstNames)] . '  ' . $lastNames[array_rand($lastNames)];
+            $position = $this->generateRandomPosition();
+            $nationality = $this->generateRandomNationality();
+            $random_detetime = Carbon::now()->subMinutes(rand(1,55));
+
+            DB::table('members')->insert([
+                'name' =>$name,
+                'oid' => rand(1,100),
+                'position' => $position,
+                'height' => rand(165,220),
+                'weight' => rand(40,90),
+                'year' => rand(1,20),
+                'age' => rand(15,59),
+                'nationality' => $nationality,
+                'created_at' => $random_detetime,
+                'updated_at' => $random_detetime        
+            ]);
+        }
+       
+    }
+}
---
+++ database/seeders/OrchestraTableSeeder.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class OrchestraTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function generateRandomName() {
+        $name = array( 'Beyond', 'S.H.E',  'Maroon',  'Westlife', 
+              'Spice Girls', 'BIGBANG', 'SUPER JUNIOR', '2NE1', 'EXO');
+              return $name[rand(0,count($name)-1)];       
+
+    }
+
+    public function generateRandomCompany() {
+        $company = array(
+            '環球音樂', '索尼音樂', '華納音樂', 'EMI', '艾德烈音樂',
+            '摩城唱片', '華納兄弟唱片', '寰宇唱片', '博德曼音樂', 
+            '卡拉OK唱片', '百代音樂', '摩登天空', '阿希姆唱片', 
+            '米高梅影視音樂', '嘉士伯唱片', '國會音樂', '貝塔斯曼音樂', 
+            '伊士曼唱片'
+          );
+          return $company[rand(0,count($company)-1)];  
+
+    }
+
+    public function generateRandomCity() {
+        $city = array(
+            ' 洛杉磯',
+            '东京',
+            '布伯班克',
+            '倫敦',
+            '台北',
+            '紐約',  
+            '布伯班克',
+            '香港',
+            '柏林',
+            '東京',
+            ' 漢堡',
+            '台北',
+            '东京',
+            '好萊塢',
+            '香港',
+            '台北',
+            ' 漢諾威',
+            '羅切斯特'
+          );
+          return $city[rand(0,count($city)-1)];  
+
+    }
+
+    public function generateRandomStyle() {
+        $style = array(
+            '流行樂', '搖滾樂', '重金屬', '嘻哈', '電子舞曲',
+            '爵士', '鄉村音樂', '藍調', '龐克', '放克',
+            '民謠', '古典樂', '另類搖滾', '迪斯可', '朋克', 
+            '放克', '英倫搖滾', '實驗音樂'
+          );
+          return $style[rand(0,count($style)-1)];  
+
+    }
+
+
+    public function run()
+    {
+        for ($i=0; $i<500; $i++) {
+            $name = $this->generateRandomName();
+            $company = $this->generateRandomCompany();
+            $city = $this->generateRandomCity();
+            $style = $this->generateRandomStyle();
+            $random_detetime = Carbon::now()->subMinutes(rand(1,55));
+
+            DB::table('orchestra')->insert([
+                'name' => $name,
+                'company' => $company,
+                'city' => $city,
+                'style'=> $style,
+                'created_at' => $random_detetime,
+                'updated_at' => $random_detetime        
+            ]);
+        }
+
+    }
+}
