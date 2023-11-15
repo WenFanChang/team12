360689223aabd1791008f34e879e9ea59bdde07e
WenFanChang <d1104181101@gm.lhu.edu.tw>
2023-11-15 10:58:15 +0800

    web 裡新增members and orchestra

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MembersController;
+use App\Http\Controllers\OrchestraController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('Members', [MembersController::class, 'index'])->name('Members.index');
+Route::get('Orchestra', [OrchestraController::class, 'index'])->name('Orchestra.index');
