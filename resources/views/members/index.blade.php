0ec73b8ce6860c157b5c0eaaa3b1b8b864cded2d
WenFanChang <d1104181101@gm.lhu.edu.tw>
2023-11-29 11:35:05 +0800

    增加板模配置 app,footer,header,並修改 members.index,及orchestras.index 等視圖

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -1,43 +1,44 @@
 
-<html>
-    <head> 
-        <title> 列出所有團員 </title>
-    </head>
-    <body>
-    <h1> 列出所有團員 </h1>
+@extends('app')
 
-    <table>
+@section('title', '樂團 網站 - 列出所有團員')
+
+@section('band_contents')
+<h1> 列出所有團員 </h1>
+
+<table>
+    <tr>
+        <th>編號</th>   
+        <th>人員名稱</th>
+        <th>團體</th>
+        <th>位置</th>
+        <th>身高</th>
+        <th>體重</th>
+        <th>年資</th>
+        <th>年齡</th>
+        <th>國籍</th>
+        <th>操作1</th>
+        <th>操作2</th>
+        <th>操作3</th>
+    </tr>
+    @for($i=0; $i<count($members); $i++)
         <tr>
-            <th>編號</th>
-            <th>人員名稱</th>
-            <th>團體</th>
-            <th>位置</th>
-            <th>身高</th>
-            <th>體重</th>
-            <th>年資</th>
-            <th>年齡</th>
-            <th>國籍</th>
-            <th>操作1</th>
-            <th>操作2</th>
-            <th>操作3</th>
+            <td>{{ $members[$i]['id'] }}</td>
+            <td>{{ $members[$i]['name'] }}</td>
+            <td>{{ $members[$i]['oid'] }}</td>
+            <td>{{ $members[$i]['position'] }}</td>
+            <td>{{ $members[$i]['height'] }}</td>
+            <td>{{ $members[$i]['weight'] }}</td>
+            <td>{{ $members[$i]['year'] }}</td>
+            <td>{{ $members[$i]['age'] }}</td>
+            <td>{{ $members[$i]['nationality'] }}</td>
+            <td><a href="{{ route('members.show', ['id'=>$members[$i]['id']]) }}">顯示</td>
+            <td><a href="{{ route('members.edit', ['id'=>$members[$i]['id']]) }}">修改</td>
+            <td>刪除</td>
+
+
         </tr>
-        @for($i=0; $i<count($members); $i++)
-            <tr>
-                <td>{{ $members[$i]['id'] }}</td>
-                <td>{{ $members[$i]['name'] }}</td>
-                <td>{{ $members[$i]['oid'] }}</td>
-                <td>{{ $members[$i]['position'] }}</td>
-                <td>{{ $members[$i]['height'] }}</td>
-                <td>{{ $members[$i]['weight'] }}</td>
-                <td>{{ $members[$i]['year'] }}</td>
-                <td>{{ $members[$i]['age'] }}</td>
-                <td>{{ $members[$i]['nationality'] }}</td>
-            </tr>
             
-        @endfor
-    </table>
-
-
-    </body>
-    
-</html>
+    @endfor
+</table>
+@endsection
--- resources/views/orchestras/index.blade.php
+++ resources/views/orchestras/index.blade.php
@@ -1,19 +1,37 @@
 
-<html>
-    <head> 
-        <title> 列出所有樂團 </title>
-    </head>
-    <body>
-    <h1> 列出所有樂團 </h1>
+@extends('app')
 
+@section('title', '樂團網站 - 列出所有樂團')
+
+@section('band_contents')
+<h1> 列出所有樂團 </h1>
+
+<table>
+    <tr>
+        <th>編號</th>   
+        <th>團體名稱</th>
+        <th>公司名稱</th>
+        <th>公司位置</th>
+        <th>曲風類別</th>
+        <th>操作1</th>
+        <th>操作2</th>
+        <th>操作3</th>
+    </tr>
     @for($i=0; $i<count($orchestras); $i++)
-        {{ $orchestras[$i]['name'] }} <br/>
-        {{ $orchestras[$i]['company'] }} <br/>
-        {{ $orchestras[$i]['city'] }} <br/>
-        {{ $orchestras[$i]['style'] }} <br/><br/>
+    <tr>
+        <td> {{ $orchestras[$i]['id'] }}</td>
+        <td> {{ $orchestras[$i]['name'] }}</td> 
+        <td> {{ $orchestras[$i]['company'] }} </td>
+        <td> {{ $orchestras[$i]['city'] }} </td>
+        <td> {{ $orchestras[$i]['style'] }} </td>
+        <td><a href="{{ route('orchestras.show', ['id'=>$orchestras[$i]['id']]) }}">顯示</td>
+        <td><a href="{{ route('orchestras.edit', ['id'=>$orchestras[$i]['id']]) }}">修改</td>
+        <td>刪除</td>
+
+    </tr>
+
+        
     @endfor
+</table>
 
-
-    </body>
-    
-</html>
+@endsection
