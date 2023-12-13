cc7bccecd42e9d5c57e48ddf716ec4e8606c9268
D1104181027 <d1104181027@gm.lhu.edu.tw>
2023-12-13 08:15:09 +0800

    字型調整

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -5,6 +5,7 @@
 @section('band_them','你所選取的團員資料')
 
 @section('band_contents')
+<h1>列出所有團員</h1>
 團員編號:{{ $member->id }}<br/>
 人員名稱:{{ $member->name }}<br/>
 團體名稱:{{ $member->oid }}<br/>
@@ -14,4 +15,7 @@
 團員年資:{{ $member->year }}<br/>
 團員年齡:{{ $member->age }}<br/>
 團員國籍:{{ $member->nationality }}<br/>
+
+<h1>{{ $member->name }}的所有球員</h1>
+
 @endsection
--- resources/views/orchestras/show.blade.php
+++ resources/views/orchestras/show.blade.php
@@ -5,6 +5,7 @@
 @section('band_them','你所選取的團員資料')
 
 @section('band_contents')
+<h1>列出所有團體</h1>
 編號:{{ $orchestra->id }}<br/>
 團體名稱:{{ $orchestra->name }}<br/>
 公司名稱:{{ $orchestra->company }}<br/>
