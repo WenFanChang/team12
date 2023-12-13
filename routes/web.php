b27a0f485901a6c433ab94e0ff8114882f62c98a
D1104181027 <d1104181027@gm.lhu.edu.tw>
2023-12-13 11:09:56 +0800

    路由錯誤修正

--- routes/web.php
+++ routes/web.php
@@ -31,14 +31,14 @@ Route::get('/', function (){
 Route::get('members', [MembersController::class, 'index'])->name('members.index');
 //顯示單一隊員資料
 Route::get('members/{id}', [MembersController::class, 'show'])->where('id', '[0-9]+')->name('members.show');
-//修改單一隊員資料
-Route::get('members/{id}/edit', [MembersController::class, 'edit'])->where('id', '[0-9]+')->name('members.edit');
 //修改團員表單
-Route::get('members/update/{id}', [MembersController::class, 'update'])->where('id', '[0-9]+')->name('members.update');
+Route::get('members/{id}/edit', [MembersController::class, 'edit'])->where('id', '[0-9]+')->name('members.edit');
+//修改團員資料
+Route::patch('members/update/{id}', [MembersController::class, 'update'])->where('id', '[0-9]+')->name('members.update');
 //刪除單一隊員資料
 Route::delete('members/delete/{id}', [MembersController::class, 'destroy'])->where('id', '[0-9]+')->name('members.destroy');
 //新增團員表單
-Route::delete('members/create', [MembersController::class, 'create'])->name('members.create');
+Route::get('members/create', [MembersController::class, 'create'])->name('members.create');
 
 
 Route::get('orchestras', [OrchestrasController::class, 'index'])->name('orchestras.index');
@@ -47,8 +47,8 @@ Route::get('orchestras/{id}', [OrchestrasController::class, 'show'])->where('id'
 
 Route::get('orchestras/{id}/edit', [OrchestrasController::class, 'edit'])->where('id', '[0-9]+')->name('orchestras.edit');
 
-Route::get('orchestras/update/{id}', [OrchestrasController::class, 'update'])->where('id', '[0-9]+')->name('orchestras.update');
+Route::patch('orchestras/update/{id}', [OrchestrasController::class, 'update'])->where('id', '[0-9]+')->name('orchestras.update');
 
 Route::delete('orchestras/delete/{id}', [OrchestrasController::class, 'destroy'])->where('id', '[0-9]+')->name('orchestras.destroy');
 
-Route::delete('orchestras/create', [OrchestrasController::class, 'create'])->name('orchestras.create');
+Route::get('orchestras/create', [OrchestrasController::class, 'create'])->name('orchestras.create');
