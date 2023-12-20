be2105b455635456659c81fff4044f7947aa535e
D1104181027 <d1104181027@gm.lhu.edu.tw>
2023-12-20 10:48:09 +0800

    orchestras的create內容修改

--- resources/views/orchestras/create.blade.php
+++ resources/views/orchestras/create.blade.php
@@ -5,7 +5,7 @@
 @section('band_theme', '建立樂團的表單')
 
 @section('band_contents')
-    {!! Form::open(['url' => 'orchestra/store']) !!}
+    {!! Form::open(['url' => 'orchestras/store']) !!}
     @include('orchestras.form', ['submitButtonText'=>"新增團隊資料"])
     {!! Form::close() !!}
 @endsection
