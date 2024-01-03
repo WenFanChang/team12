1965aefe36b37301fcc49edbcdea671a8ab9bd63
D1104181027 <d1104181027@gm.lhu.edu.tw>
2024-01-03 14:32:40 +0800

    更新兩個views： players.index 和teams.index，讓他們針對admin 與 manager 呈現受控管的超連結(新增、修改、刪除)

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -29,8 +29,12 @@
         <th>年齡</th>
         <th>國籍</th>
         <th>操作1</th>
+        @can('admin')
         <th>操作2</th>
         <th>操作3</th>
+        @elsecan('manager')
+        <th>操作2</th>
+        @endcan
     </tr>
     @foreach ($members as $member)
         <tr>
@@ -44,6 +48,7 @@
             <td>{{ $member->age }} </td>
             <td>{{ $member->nationality }} </td>
             <td><a href="{{ route('members.show', ['id'=>$member->id]) }}">顯示</a></td>
+            @can('admin')
             <td><a href="{{ route('members.edit', ['id'=>$member->id]) }}">修改</a></td>
             <td>
                 <form action="{{ url('/members/delete', ['id' => $member->id]) }}" method="post">
@@ -52,6 +57,9 @@
                     @csrf
                 </form>
             </td>
+            @elsecan('manager')
+            <td><a href="{{ route('members.edit', ['id'=>$member->id]) }}">修改</a></td>
+            @endcan
         </tr>
     @endforeach
 </table>
--- resources/views/orchestras/index.blade.php
+++ resources/views/orchestras/index.blade.php
@@ -4,7 +4,9 @@
 
 @section('band_contents')
 <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+    @can('admin')
     <a href="{{ route('members.create') }} ">新增樂團</a>
+    @endcan
     <a href="{{ route('members.index') }} ">所有樂團</a>
 </div>
 
@@ -16,8 +18,12 @@
         <th>公司位置</th>
         <th>曲風類別</th>
         <th>操作1</th>
+        @can('admin')
         <th>操作2</th>
         <th>操作3</th>
+        @elsecan('manager')
+        <th>操作2</th>
+        @endcan
     </tr>
     @foreach ($orchestras as $orchestra)
         <tr>
@@ -27,6 +33,7 @@
             <td>{{ $orchestra->city }} </td>
             <td>{{ $orchestra->style }} </td>
             <td><a href="{{ route('orchestras.show', ['id'=>$orchestra->id]) }}">顯示</a></td>
+            @can('admin')
             <td><a href="{{ route('orchestras.edit', ['id'=>$orchestra->id]) }}">修改</a></td>
             <td>
                 <form action="{{ url('/orchestras/delete', ['id' => $orchestra->id]) }}" method="post">
@@ -35,6 +42,9 @@
                     @csrf
                 </form>
             </td>
+            @elsecan('manager')
+            <td><a href="{{ route('orchestras.edit', ['id'=>$orchestra->id]) }}">修改</a></td>    
+            @endcan
         </tr>
     @endforeach
 </table>
