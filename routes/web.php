2fb60caa9c4d8386c3ad9710ee9f29aee31554c9
micahael123321123 <d1104181048@gm.lhu.edu.tw>
2024-01-03 14:35:51 +0800

    1/3上課內容

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::post('orchestras/store', [OrchestrasController::class, 'store'])->name('o
 
 
 
+Auth::routes();
 
-
-
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
