f792425e60462f2014bc519d7562761d9e885e28
WenFanChang <d1104181101@gm.lhu.edu.tw>
2024-01-03 14:26:32 +0800

    更新兩個views： players.index 和teams.index，讓他們針對admin 與 manager 呈現受控管的超連結(新增、修改、刪除)

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -6,7 +6,9 @@
 @section('band_contents')
 <h1> 列出所有團員 </h1>
 <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-1">
+    @can('admin')
     <a href="{{ route('members.create') }}">新增團員</a>
+    @endcan
     <a href="{{ route('members.index') }} ">所有團員</a>
     <a href="{{ route('members.senior') }} ">資深團員</a>
     <form action="{{ url('members/position') }}" method='GET'>
@@ -14,7 +16,8 @@
         {!! Form::select('pos', $positions, $selectedPosition, ['class' => 'form-control']) !!}
         <input class="btn btn-default" type="submit" value="查詢" />    
         @csrf
-    </form>    
+    </form>
+        
 </div>
 
 <table>
@@ -29,8 +32,12 @@
         <th>年齡</th>
         <th>國籍</th>
         <th>操作1</th>
+        @can('admin')
         <th>操作2</th>
         <th>操作3</th>
+        @elsecan('manager')
+        <th>操作2</th>
+        @endcan
     </tr>
     
     @foreach ($members as $member)
@@ -45,6 +52,7 @@
             <td>{{ $member->age }}</td>
             <td>{{ $member->nationality }}</td>
             <td><a href="{{ route('members.show', ['id'=>$member->id]) }}">顯示</a></td>
+            @can('admin')
             <td><a href="{{ route('members.edit', ['id'=>$member->id]) }}">修改</a></td>
             <td>
                 <form action="{{ url('/members/delete', ['id' => $member->id]) }}" method="post">
@@ -53,6 +61,9 @@
                     @csrf
                 </form>
             </td>
+            @elsecan('manager')
+            <td><a href="{{ route('members.edit', ['id'=>$member->id]) }}">修改</a></td>
+            @endcan
         </tr>
     @endforeach
 </table>
--- resources/views/orchestras/index.blade.php
+++ resources/views/orchestras/index.blade.php
@@ -6,7 +6,9 @@
 @section('band_contents')
 <h1> 列出所有樂團 </h1>
 <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-1">
+    @can('admin')
     <a href="{{ route('orchestras.create') }}">新增樂團</a>
+    @endcan
     <a href="{{ route('orchestras.index') }} ">所有樂團</a>
 
 </div>
@@ -19,8 +21,12 @@
         <th>公司位置</th>
         <th>曲風類別</th>
         <th>操作1</th>
+        @can('admin')
         <th>操作2</th>
         <th>操作3</th>
+        @elsecan('manager')
+        <th>操作2</th>
+        @endcan
     </tr>
     @foreach ($orchestras as $orchestra)
     <tr>
@@ -30,6 +36,7 @@
         <td> {{ $orchestra->city }} </td>
         <td> {{ $orchestra->style }} </td>
         <td><a href="{{ route('orchestras.show', ['id'=>$orchestra->id]) }}">顯示</a></td>
+        @can('admin')
         <td><a href="{{ route('orchestras.edit', ['id'=>$orchestra->id]) }}">修改</a></td>
         <td>
             <form action="{{ url('/orchestras/delete', ['id' => $orchestra->id]) }}" method="post">
@@ -38,6 +45,9 @@
                 @csrf
             </form>
         </td>
+        @elsecan('manager')
+            <td><a href="{{ route('orchestras.edit', ['id'=>$orchestra->id]) }}">修改</a></td>    
+            @endcan
 
     </tr>
 
